65dbb0d9c6e2af4eda0fb9d24ca41348d938a918
Oliver Morgan <wv19787@bristol.ac.uk>
2023-12-01 12:45:35 +0000

    corrected author

--- ca_fire_growth_experiment.py
+++ ca_fire_growth_experiment.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Oct  4 20:35:51 2022
+Created on Fri Dec  1 20:35:51 2022
 
-@author: steph
+@author: Oliver
 """
 from ca_fire import evolve_forest, write_netcdf
 
